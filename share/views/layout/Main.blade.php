3adcac505a7a1833bd63758c1096c834b89cf475
syo <syo_shu0@yahoo.co.jp>
2018-02-11 20:43:27 +0900

    googleアナリティクスを追加

--- share/views/layout/Main.blade.php
+++ share/views/layout/Main.blade.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id={{getenv('GOOGLE_ANALYTICS')}}"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '{{getenv('GOOGLE_ANALYTICS')}}');
+    </script>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>{{getenv('APP_NAME')}} - @yield('title')</title>
